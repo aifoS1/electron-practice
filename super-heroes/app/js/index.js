3085af400584e447077327096df2c7138f80e4a3
Sofia Petsoulakis <petsofia@gmail.com>
2017-01-06 13:12:44 -0500

    directory for first simple electron tutorial

---
+++ super-heroes/app/js/index.js
@@ -0,0 +1,35 @@
+'use strict';
+//for .env file to work
+require('dotenv').config();
+var md5 = require('md5');
+var publicKey = process.env.MARVEL_PUBLIC_KEY;
+var privateKey = process.env.MARVEL_PRIVATE_KEY;
+var ts = String(new Date().getTime());
+var hash = md5(ts+privateKey+publicKey);
+
+// var url = 'https://gateway.marvel.com/v1/public/characters?ts='+ ts+ '&apikey='+ publicKey+ '&hash=' + hash + '&limit=25';
+
+var url = 'https://gist.githubusercontent.com/ChrisChinchilla/29486e8ce367f426dfe6b15dbcc3fa54/raw/3ea92af51ce3749bb5983c1cb0359883592daef6/Marvel%2520Electron%2520Data';
+
+// create the HTML and placeholder variables needed for outputting each character into the character_list div:
+var characterHTML = (character)=> `
+  <div class="character">
+    <h2>${character.name}</h2>
+    <img src="${character.thumbnail.path}.${character.thumbnail.extension}" />
+  </div>`;
+
+fetch(url)
+  .then(resp => resp.json())
+  .then(json => json.data.results)
+  .then(characters => {
+      var html = characters.map(characterHTML).join('');
+      var characterList = document.getElementById("character_list");
+      characterList.innerHTML = html;
+
+      new Notification(document.title, {
+          body: 'Super Heroes Loaded!'
+      });
+  })
+  .catch((error)=> {
+      console.error(error);
+  });
